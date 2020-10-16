32c9bf3bf353703028da72a397bd0221b5d313a9
Noah Skelton <skelton.noah@gmail.com>
2020-10-16 17:33:45 +0100

    execution stats working

--- analyze.py
+++ analyze.py
@@ -2,7 +2,9 @@ import subprocess
 from multiprocessing import Process, Array, Pool, Manager
 import random
 arr2 = []
-
+import time
+import datetime
+import pprint
 
 def stress_test(duration: int, d: dict) -> None:
     """
@@ -12,14 +14,25 @@ def stress_test(duration: int, d: dict) -> None:
     you can re-assign the modified object to the container proxy.
     https://docs.python.org/2/library/multiprocessing.html#multiprocessing.managers.SyncManager.list
     """
+    start_time = time.time()
     stress_test = subprocess.Popen(["python","stress.py",
                                     str(duration)],
                                    stdout=subprocess.PIPE)
     stress_test_output = stress_test.communicate()[0].decode('utf-8').split()[4:]
+    end_time = time.time()
+    total_time = end_time - start_time
+    start_time = datetime.datetime.fromtimestamp(start_time).strftime('%c')
+    end_time = datetime.datetime.fromtimestamp(end_time).strftime('%c')
     throughput_values = stress_test_output[0::2]
     latency_values = stress_test_output[1::2]
     d["throughput"].extend(throughput_values)
     d["latency"].extend(latency_values)
+    pid = stress_test.pid
+    d['execution_stats'].append("pid: {} started at {} and finished at {}"
+                                " and took {} seconds"
+                                .format(pid,start_time,end_time, total_time)
+                                )
+    print(stress_test.pid)
 
 if __name__ == '__main__':
     def run_processes_in_parallel(function, num_threads):
@@ -27,6 +40,7 @@ if __name__ == '__main__':
         shared_dict = manager.dict()
         shared_dict["throughput"] = manager.list()
         shared_dict["latency"] = manager.list()
+        shared_dict["execution_stats"] = manager.list()
         processes = []
         for i in range(num_threads):
             duration = random.randrange(1, 7)
@@ -38,5 +52,6 @@ if __name__ == '__main__':
 
         print(shared_dict["throughput"][:])
         print(shared_dict["latency"][:])
-    run_processes_in_parallel(stress_test, 10)
+        pprint.pprint((shared_dict["execution_stats"][:]))
+    run_processes_in_parallel(stress_test, 5)
    # stress_test(4,{})
