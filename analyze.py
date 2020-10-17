538453b35ff1f82221cd102c19fc4094213ed642
Noah Skelton <skelton.noah@gmail.com>
2020-10-17 13:06:29 +0100

    CSV writing working and argparse working

--- analyze.py
+++ analyze.py
@@ -7,48 +7,68 @@ import pprint
 import argparse
 import statistics
 from typing import Callable, Dict, List
+import csv
+import io
 
-# parser = argparse.ArgumentParser(
-#     description="Spawn multiple stress processes for ScyllaDB and get metrics"
-# )
-# parser.add_argument(
-#     "number_of_processes", help="Number of processes to spawn", type=int
-# )
-# parser.add_argument(
-#     "max_duration_of_processes", help="Max duration of processes", type=int
-# )
-# args = parser.parse_args()
+parser = argparse.ArgumentParser(
+    description="Spawn multiple stress processes for ScyllaDB and get metrics"
+)
+parser.add_argument(
+    "threads",
+    help="Number of processes to spawn (integer), must be less than or equal"
+         " to max duration value to ensure unique randomness values.",
+    type=int,
+)
+parser.add_argument(
+    "max_duration", help="Max duration of processes (integer)", type=int
+)
+
+
+args = parser.parse_args()
+
+if args.threads > args.max_duration:
+    parser.error(
+        "Number of threads must be less than or equal to max duration value"
+    )
 
 
 def stress_test(duration: int, shared_dict: dict) -> None:
 
-    start_time = time.time()
-    stress_test = subprocess.Popen(
-        ["python", "stress.py", str(duration)], stdout=subprocess.PIPE
-    )
-    stress_test_output = stress_test.communicate()[0].decode("utf-8").split()[4:]
-    end_time = time.time()
-    total_time = end_time - start_time
+    start_time = time.monotonic()
 
+    stress_test = subprocess.Popen(
+        ["python", "stress.py", str(duration)],
+        stdout=subprocess.PIPE,
+    )
+    output_bytes = stress_test.communicate()[0]
+    output_string = output_bytes.decode()
+    output_file = io.StringIO(output_string)
+
+    csv_reader = csv.DictReader(output_file)
+    rows = [row for row in csv_reader]
+    throughput_values_int = [int(row["Throughput (ops/s)"]) for row in rows]
+    latency_values_int = [int(row["Latency (ms)"]) for row in rows]
+    end_time = time.monotonic()
+    total_time = end_time - start_time
     start_time_str = datetime.datetime.fromtimestamp(start_time).strftime("%c")
     end_time_str = datetime.datetime.fromtimestamp(end_time).strftime("%c")
-    throughput_values = stress_test_output[0::2]
-    latency_values = stress_test_output[1::2]
-    throughput_values_int = [int(i) for i in throughput_values]
-    latency_values_values_int = [int(i) for i in latency_values]
+
     shared_dict["throughput"].extend(throughput_values_int)
-    shared_dict["latency"].extend(latency_values_values_int)
+    shared_dict["latency"].extend(latency_values_int)
     pid = stress_test.pid
     shared_dict["execution_stats"].append(
         "pid: {} started at {} and finished at {}"
-        " and took {} seconds".format(pid, start_time_str, end_time_str, total_time)
+        " and took {} seconds".format(
+            pid, start_time_str, end_time_str, total_time
+        )
     )
-    print(stress_test.pid)
 
 
-def run_processes_in_parallel(function: Callable[[int, dict], None], num_threads: int, max_duration: int) -> Dict:
+def run_processes_in_parallel(
+    function: Callable[[int, dict], None], num_threads: int, max_duration: int
+) -> Dict:
     manager = Manager()
-    shared_dict = manager.dict() # type: ignore
+    shared_dict = manager.dict()  # type: ignore
     shared_dict["throughput"] = manager.list()
     shared_dict["latency"] = manager.list()
     shared_dict["execution_stats"] = manager.list()
@@ -62,18 +82,21 @@ def run_processes_in_parallel(function: Callable[[int, dict], None], num_threads
     for process in processes:
         process.join()
 
-    return dict(shared_dict)
+    return shared_dict
 
 
 class CalculateMetrics:
     def __init__(
-        self, throughput_metrics: List[int], latency_metrics: List[int]):
+        self, throughput_metrics: List[int], latency_metrics: List[int]
+    ):
         self.throughput_metrics = throughput_metrics
         self.latency_metrics = latency_metrics
 
     def average_throughput(self) -> float:
 
-        throughput_average = sum(self.throughput_metrics) / len(self.throughput_metrics)
+        throughput_average = sum(self.throughput_metrics) / len(
+            self.throughput_metrics
+        )
         return throughput_average
 
     def average_latency(self) -> float:
@@ -97,25 +120,25 @@ class CalculateMetrics:
         return latency_min
 
     def ninety_fifth_percentile_throughput(self) -> float:
-        percentile_throughput = (
-            statistics.quantiles(self.throughput_metrics, n=20)[-1]
-        )
+        percentile_throughput = statistics.quantiles(
+            self.throughput_metrics, n=20
+        )[-1]
         return percentile_throughput
 
     def ninety_fifth_percentile_latency(self) -> float:
-        percentile_latency = (
-            statistics.quantiles(self.latency_metrics, n=20)[-1]
-        )
+        percentile_latency = statistics.quantiles(self.latency_metrics, n=20)[
+            -1
+        ]
         return percentile_latency
 
-class CLILineCreator:
 
+class CLILineCreator:
     def __init__(self, throughput_metrics: list, latency_metrics: list):
         self._metrics_calculator = CalculateMetrics(
             throughput_metrics=throughput_metrics,
             latency_metrics=latency_metrics,
         )
-        #self._execution_stats = execution_stats
+        # self._execution_stats = execution_stats
 
     def average(self) -> str:
         average_throughput = self._metrics_calculator.average_throughput()
@@ -145,33 +168,40 @@ class CLILineCreator:
         return output_string
 
     def percentile(self) -> str:
-        percentile_throughput = self._metrics_calculator.ninety_fifth_percentile_throughput()
-        percentile_latency = self._metrics_calculator.ninety_fifth_percentile_latency()
+        percentile_throughput = (
+            self._metrics_calculator.ninety_fifth_percentile_throughput()
+        )
+        percentile_latency = (
+            self._metrics_calculator.ninety_fifth_percentile_latency()
+        )
         output_string = (
             f"Throughput 95th percentile = {percentile_throughput} ops/s\n"
             f"Latency 95th percentile = {percentile_latency}ms"
-            )
+        )
 
         return output_string
 
+
 if __name__ == "__main__":
-    # output_data = run_processes_in_parallel(
-    #     function=stress_test,
-    #     max_duration=args.max_duration_of_processes,
-    #     num_threads=args.number_of_processes,
-    # )
-    #
     output_data = run_processes_in_parallel(
         function=stress_test,
-        max_duration=6,
-        num_threads=3,
+        max_duration=args.max_duration,
+        num_threads=args.threads,
     )
+
+    # output_data = run_processes_in_parallel(
+    #     function=stress_test,
+    #     max_duration=3,
+    #     num_threads=3,
+    # )
     throughput = output_data["throughput"][:]
     latency = output_data["latency"][:]
     execution_stats = output_data["execution_stats"][:]
 
-    calculator = CLILineCreator(throughput_metrics=throughput,latency_metrics=latency)
-
+    calculator = CLILineCreator(
+        throughput_metrics=throughput, latency_metrics=latency
+    )
+    #
     print(calculator.average())
     print(calculator.max())
     print(calculator.min())
--- stress.py
+++ stress.py
@@ -3,6 +3,7 @@ import sys
 import time
 import random
 import argparse
+import csv
 
 parser = argparse.ArgumentParser(description="Simulate stress on ScyllaDB")
 parser.add_argument("duration", help="Duration of stress process", type=int)
@@ -11,19 +12,16 @@ args = parser.parse_args()
 
 def simulate_stress(stress_duration: int) -> None:
     headers = ["Throughput (ops/s)", "Latency (ms)"]
-    sys.stdout.write(format_list(headers))
     start_time = time.monotonic()
+    writer = csv.DictWriter(sys.stdout, fieldnames=headers)
+    writer.writeheader()
+
     while time.monotonic() - start_time < stress_duration:
         throughput = random.randrange(0, 100000)
         latency = random.randrange(0, 20000)
-        output_string = format_list([throughput, latency])
-        sys.stdout.write(output_string)
+        row = {"Throughput (ops/s)": throughput, "Latency (ms)": latency}
+        writer.writerow(row)
         time.sleep(1)
-
-
-def format_list(input_values: list) -> str:
-    output = "{:<20}  {:<20}".format(input_values[0], input_values[1])
-    return output + "\n"
 
 
 simulate_stress(args.duration)
